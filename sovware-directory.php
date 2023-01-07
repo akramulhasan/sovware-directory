de595699b456d394e753b8099d772dd353609a89
AKRAMUL HASAN <akramul@AKRAMULs-MacBook-Pro.local>
2023-01-07 09:08:45 +0600

    very first file created and initialized git

---
+++ sovware-directory.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Plugin Name: Sovware Directory
+ * Plugin Url: https://www.akramuldev.com/plugins/sovware-directory
+ * Version: 1.0.0
+ * Description: This is a directory plugin
+ */
+
+?>
